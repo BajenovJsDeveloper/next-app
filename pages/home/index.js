09406fa9080092214e9de9ea5b0fcc2f38f0c636
BajenovJsDeveloper <vladimir.bajenov@feel-it-services.com>
2021-07-30 16:27:22 +0300

    next-project

---
+++ pages/home/index.js
@@ -0,0 +1,30 @@
+import axios from 'axios'
+import Head from 'next/head'
+
+export default function Home ({ posts }) {
+  if(!posts) return null
+  return (
+    <div className>
+      <Head>
+        <meta name="Next test" content="Next, test, reactjs"/>
+        <title>Next js test page</title>
+      </Head>
+      <h2 className="p-5 text-gray-700 font-extrabold text-3xl text-center"> Home page</h2>
+      { posts.map(item => (
+        <div key={ item.id } className="container mx-auto p-4 border rounded-md my-2">
+          <h3 className="text-2xl text-center font-extrabold mb-2 text-blue-700">#{item.id} { item.title.slice(0,1).toUpperCase() +  item.title.slice(1)}</h3>
+          <p className="text-gray-400">{ item.body }</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export const getStaticProps = async (ctx) => {
+  const posts = await axios('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  return { 
+    props: {
+      posts: posts.data
+    }  
+  }
+}
